ba7c0bfc7fa7471f542b7a9a90c88011687b8991
unknown <Administrator@.(none)>
2012-10-08 07:36:41 -0500

    Initial

---
+++ FixRes/FixResForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FixRes
+{
+    partial class FixResForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtResPath
+            // 
+            this.txtResPath.Location = new System.Drawing.Point(161, 12);
+            this.txtResPath.Name = "txtResPath";
+            this.txtResPath.Size = new System.Drawing.Size(385, 20);
+            this.txtResPath.TabIndex = 0;
+            this.txtResPath.TextChanged += new System.EventHandler(this.txtResPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path to EntityResources.resx";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(491, 38);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(55, 22);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Enabled = false;
+            this.btnPreview.Location = new System.Drawing.Point(390, 458);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 3;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(471, 458);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(15, 70);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(531, 382);
+            this.txtContent.TabIndex = 5;
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(15, 46);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(450, 21);
+            this.lblError.TabIndex = 6;
+            this.lblError.Text = "Bla bla bla";
+            this.lblError.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Resource Files|*.resx|All Files|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // FixResForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 493);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResPath);
+            this.Name = "FixResForm";
+            this.Text = "FixResForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ FixRes/FixResForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace FixRes
+{
+    public partial class FixResForm : Form
+    {
+        public FixResForm()
+        {
+            InitializeComponent();
+        }
+
+        #region UI
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (openFileDialog1.FileName != null)
+            {
+                txtResPath.Text = openFileDialog1.FileName;
+                ValidateFile();
+            }
+        }
+
+        private void txtResPath_TextChanged(object sender, EventArgs e)
+        {
+            ValidateFile();
+        }
+
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            PreviewConvert();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (PreviewConvert())
+            {
+                try
+                {
+                    File.WriteAllText(txtResPath.Text, txtContent.Text);
+                    lblError.Visible = false;
+                    MessageBox.Show("File saved");
+                }
+                catch (Exception x)
+                {
+                    lblError.Text = "Could not write file: " + x.Message;
+                    lblError.Visible = true;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Logic
+
+        private void ValidateFile()
+        {
+            String fileName = txtResPath.Text;
+            btnPreview.Enabled = false;
+            btnSave.Enabled = false;
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    txtContent.Text = File.ReadAllText(fileName);
+                    lblError.Visible = false;
+                    btnPreview.Enabled = true;
+                    btnSave.Enabled = true;
+                }
+                catch (Exception x)
+                {
+                    lblError.Text = "File cannot be read: " + x.Message;
+                    lblError.Visible = true;
+                }
+            }
+            else
+            {
+                lblError.Text = "File does not exist";
+                lblError.Visible = true;
+                txtContent.Text = "";
+            }
+        }
+
+        private bool PreviewConvert()
+        {
+            try
+            {
+                txtContent.Text = FixResource(txtResPath.Text);
+                lblError.Visible = false;
+                return true;
+            }
+            catch (Exception x)
+            {
+                lblError.Text = "Could not convert file: " + x.Message;
+                lblError.Visible = true;
+            }
+            return false;
+        }
+
+        private string FixResource(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            Regex rxName = new Regex(@".*\.(.*)\.DisplayName$");
+
+            foreach (XmlNode node in doc.SelectNodes("//root/data/value"))
+            {
+                if (String.IsNullOrEmpty(node.InnerText) || node.InnerText == "Sage.Platform.Orm.Entities.OrmFieldProperty")
+                {
+                    String name = ((XmlElement)node.ParentNode).GetAttribute("name");
+                    Match m = rxName.Match(name);
+                    if (m.Success)
+                    {
+                        node.InnerText = m.Groups[1].Value;
+                    }
+                }
+            }
+            using (StringWriter buf = new StringWriter())
+            {
+                XmlWriterSettings settings = new XmlWriterSettings { Indent = true };
+                using (XmlWriter w = XmlWriter.Create(buf, settings))
+                {
+                    doc.WriteTo(w);
+                }
+                return buf.GetStringBuilder().ToString();
+            }
+        }
+
+        #endregion
+
+    }
+}
